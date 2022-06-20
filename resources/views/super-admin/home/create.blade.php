89860a1a6c0251d14038e7dc4e4d9b863da1cb65
fateme mozafari <fatemem1990@yahoo.com>
2022-06-20 14:24:53 +0430

    add home blade and model and controller

---
+++ resources/views/super-admin/home/create.blade.php
@@ -0,0 +1,51 @@
+<div style="padding-right: 20px;padding-left: 20px;font-size: larger">
+    <div class="alert alert-info" role="alert" >
+        <strong><b>ویرایش صفحه اصلی</b></strong>
+    </div>
+</div>
+<div class="row" style="padding-right: 20px;padding-left: 20px">
+    <div class="col-12">
+        <div class="card m-b-30">
+            <div class="card-body">
+                <div style="font-size: medium">
+                    تصاویر صفحه اصلی
+                </div>
+                <div style="padding-bottom: 20px">
+                    در صورت حذف تصاویر حد اقل یک تصویر باقی بماند . سایز تصاویر باید یکسان باشد (800 * 300 pixels)
+
+                </div>
+                <div class="row">
+
+                    <div class="col-lg-4">
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid mx-auto" style="width:100%"  src="{{asset('zinzer/assets/images/small/0001.jpg')}}" alt="اسلاید اول">
+                        </div>
+                        <div class="button-items" style="text-align: left">
+                            <button type="button" class="btn btn-info waves-effect waves-light">جا به جایی</button>
+                            <button type="button" class="btn btn-danger waves-effect waves-light">پاک کردن</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid mx-auto" style="width:100%"  src="{{asset('zinzer/assets/images/small/0002.jpg')}}" alt="اسلاید اول">
+                        </div>
+                        <div class="button-items" style="text-align: left">
+                            <button type="button" class="btn btn-info waves-effect waves-light">جا به جایی</button>
+                            <button type="button" class="btn btn-danger waves-effect waves-light">پاک کردن</button>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid mx-auto" style="width:100%"  src="{{asset('zinzer/assets/images/small/0003.jpg')}}" alt="اسلاید اول">
+                        </div>
+                        <div class="button-items" style="text-align: left">
+                            <button type="button" class="btn btn-info waves-effect waves-light">جا به جایی</button>
+                            <button type="button" class="btn btn-danger waves-effect waves-light">پاک کردن</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
