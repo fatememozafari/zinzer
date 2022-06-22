efa84b363d6e0b9c4b9a0715d614781d989d5b00
fm <fatemem1990@yahoo.com>
2022-06-22 17:05:38 +0430

    add & edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use \Illuminate\Support\Facades\Route;
-
-
+use \App\Http\Controllers\SuperAdmin\AboutController;
+use \App\Http\Controllers\SuperAdmin\ContactController;
+use \App\Http\Controllers\SuperAdmin\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +17,14 @@ use \Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 // Super admin Routes
 Route::group(['prefix' => 'super-admin', 'namespace' => 'SuperAdmin', 'middleware' => 'auth'], function () {
     Route::get('/manager-dashboard', 'ManagerDashboardController@index')->name('super-admin.manager-dashboard')->middleware('auth');
+
 });
+
+
 
 // Admin Routes
 Route::group(['prefix' => 'admin', 'namespace' => 'admin', 'middleware' => 'auth'], function () {
@@ -51,6 +55,8 @@ Route::group(['prefix' => 'admin', 'namespace' => 'admin', 'middleware' => 'auth
 // auth Routes
 Route::group(['prefix' => 'auth', 'namespace' => 'auth'], function () {
     Route::get('/register', 'RegisterController@registerForm')->name('auth.register.form');
+    Route::post('/register', 'RegisterController@register')->name('auth.register');
+
     Route::get('/login', 'LoginController@loginForm')->name('auth.login.form');
     Route::post('/login', 'LoginController@login')->name('auth.login');
     Route::get('/send-code', 'LoginController@sendCode')->name('auth.send.code');
@@ -58,23 +64,73 @@ Route::group(['prefix' => 'auth', 'namespace' => 'auth'], function () {
 
 });
 
+
+
 // zinzer Routes
 
 Route::group(['prefix' => 'zinzer', 'namespace' => 'zinzer'], function () {
     Route::view('/home', '/zinzer/home')->name('zinzer.home');
     Route::view('/contact', '/zinzer/contact')->name('zinzer.contact');
-    Route::view('/about', '/zinzer/about')->name('zinzer.about');
 
 });
 
+// about Routes
+
+Route::get('zinzer/about', [AboutController::class,'show'])->name('zinzer.about');
+Route::get('/super-admin/about',[AboutController::class,'index']);
+Route::get('/super-admin/about/create',[AboutController::class,'create']);
+Route::post('/super-admin/about/create',[AboutController::class,'store']);
+Route::get('/super-admin/about/{id}/edit',[AboutController::class,'edit']);
+Route::put('/super-admin/about/{id}',[AboutController::class,'update']);
+
+
+// contact Routes
+Route::get('zinzer/contact', [ContactController::class,'show'])->name('zinzer.contact');
+Route::get('/super-admin/contact',[ContactController::class,'index']);
+Route::get('/super-admin/contact/create',[ContactController::class,'create']);
+Route::post('/super-admin/contact/create',[ContactController::class,'store']);
+Route::get('/super-admin/contact/{id}/edit',[ContactController::class,'edit']);
+Route::put('/super-admin/contact/{id}',[ContactController::class,'update']);
+
+
+// home Routes
+Route::get('/', [HomeController::class,'show']);
+Route::get('/zinzer/home', [HomeController::class,'show'])->name('zinzer.home');
+Route::get('/super-admin/home',[HomeController::class,'index']);
+Route::get('/super-admin/home/create',[HomeController::class,'create']);
+Route::post('/super-admin/home/create',[HomeController::class,'store']);
+Route::get('/super-admin/home/{id}/edit',[HomeController::class,'edit']);
+Route::put('/super-admin/home/{id}',[HomeController::class,'update']);
+Route::delete('super-admin/home/{id}', [HomeController::class,'destroy'])->name('home.delete')->middleware('auth');
+
+// article Routes
+Route::get('super-admin/article', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'index'])->name('article.index');
+Route::get('zinzer/article', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'show'])->name('zinzer.article');
+Route::get('super-admin/article/{id}/edit', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'edit'])->name('article')->middleware('auth');
+Route::put('super-admin/article/{id}', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'update'])->name('article.update')->middleware('auth');
+Route::get('super-admin/article/create', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'create'])->name('article.create')->middleware('auth');
+Route::post('super-admin/article', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'store'])->name('article.store')->middleware('auth');
+Route::delete('super-admin/article/{id}', [\App\Http\Controllers\SuperAdmin\ArticleController::class,'destroy'])->name('article.delete')->middleware('auth');
+
+
+// news Routes
+Route::get('super-admin/news', [\App\Http\Controllers\SuperAdmin\NewsController::class,'index'])->name('news.index');
+Route::get('zinzer/news', [\App\Http\Controllers\SuperAdmin\NewsController::class,'show'])->name('zinzer.news');
+Route::get('super-admin/news/{id}/edit', [\App\Http\Controllers\SuperAdmin\NewsController::class,'edit'])->name('news')->middleware('auth');
+Route::put('super-admin/news/{id}', [\App\Http\Controllers\SuperAdmin\NewsController::class,'update'])->name('news.update')->middleware('auth');
+Route::get('super-admin/news/create', [\App\Http\Controllers\SuperAdmin\NewsController::class,'create'])->name('news.create')->middleware('auth');
+Route::post('super-admin/news', [\App\Http\Controllers\SuperAdmin\NewsController::class,'store'])->name('news.store')->middleware('auth');
+Route::delete('super-admin/news/{id}', [\App\Http\Controllers\SuperAdmin\NewsController::class,'destroy'])->name('news.delete')->middleware('auth');
 
 
 
 
+Route::get('/aa',function (Request $request)
+{ $mobile = $request->input('mobile');
+    $type[]=User::query()->select('type')->where('mobile', $mobile)->get();
+dd($type);
 
-
-
-
+});
 
 
 
