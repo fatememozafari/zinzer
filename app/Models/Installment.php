b8161c0a6b9569cd6bbbfb825b42ed4c0b4b4f24
fm <fatemem1990@yahoo.com>
2022-06-24 20:47:16 +0430

    add models

---
+++ app/Models/Installment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Installment extends Model
+{
+    use HasFactory;
+
+
+    protected $guarded = ['id'];
+
+    // installments count
+    const CASH = 'cash';
+    const ONE_INSTALLMENT = 'One_Installment';
+    const TWO_INSTALLMENT = 'Two_Installment';
+    const THREE_INSTALLMENT = 'Three_Installment';
+
+
+    // types
+    const STUDIES = 'studies';
+    const MEMBERSHIP = 'membership';
+
+
+    // status
+    const PAID = 'paid';
+    const UNPAID = 'Unpaid';
+    const EXPIRED = 'Expired';
+
+
+}
